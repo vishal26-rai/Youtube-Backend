e477651bd287527efad298b10bc30a9b6ee68eba
vishal26-rai <vishalrai.2607@gmail.com>
2025-01-11 01:53:39 +0530

    Implement channel statistics and video retrieval in dashboard controller; add tweet creation, update, and deletion functionality in tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,6 +7,22 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content } = req.body;
+
+    if(!content){
+        throw new ApiError(200,"Content is required")
+    }
+
+    const tweet = await Tweet.create({
+        content,
+        owner: req.user?._id
+    });
+   
+    if(!tweet){
+        throw new ApiError(500,"Tweet not created Please try again")
+    }
+
+    return res.status(201).json(new ApiResponse(201,tweet,"Tweet created successfully"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
@@ -15,11 +31,58 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {content} = req.body;
+    const {tweetId} = req.params;
+
+    if(!content){
+        throw new ApiError(400,"Content is required")
+    }
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400,"Invalid tweet id")
+    }
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet){
+        throw new ApiError(404,"Tweet not found")
+    }
+
+    if(tweet?.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403,"You are not the owner of this tweet")
+    }
+    const newtweet = tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set:{
+                content
+            }
+        },
+        {
+            new:true
+        }
+    );
+    if(!newtweet){
+        throw new ApiError(500,"Tweet not updated")
+    }
+    return res.status(200).json(new ApiResponse(200,newtweet,"Tweet updated successfully"));
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
-})
+    const {tweetId} = req.params;
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400,"Invalid tweet id")
+    }
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(404,"Tweet not found")
+    }
+    if(tweet?.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403,"You are not the owner of this tweet")
+    }
+    await Tweet.findByIdAndDelete(tweetId);
+    return res.status(200).json(new ApiResponse(200,{}, "Tweet deleted successfully"))
+});
 
 export {
     createTweet,
